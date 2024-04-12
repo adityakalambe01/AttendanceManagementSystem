bb177b4bb089ffa1c2de8241fdcd78b6e96f5228
Aditya Kalambe <adityakalambe20@gmail.com>
2024-04-12 06:54:18 +0530

    Student Entity

---
+++ src/main/java/com/attendancemanagementsystem/entity/Student.java
@@ -0,0 +1,27 @@
+package com.attendancemanagementsystem.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "students")
+public class Student {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long studentId;
+
+    @Column(name = "name")
+    private String studentName;
+
+    @Column(name = "email")
+    private String studentEmailId;
+
+    @Column(name = "dept_id")
+    private String departmentId;
+}
