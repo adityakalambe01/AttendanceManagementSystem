43ecef5799b68dbf87e299130ca8267f019db269
Aditya Kalambe <adityakalambe20@gmail.com>
2024-04-12 19:41:39 +0530

    User Exception

---
+++ src/main/java/com/attendancemanagementsystem/customexceptions/UserException.java
@@ -0,0 +1,11 @@
+package com.attendancemanagementsystem.customexceptions;
+
+public class UserException extends Exception{
+    public UserException(){
+        super();
+    }
+    public UserException(String message){
+        super(message);
+    }
+
+}
