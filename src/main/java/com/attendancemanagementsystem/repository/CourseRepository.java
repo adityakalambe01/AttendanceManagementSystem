25a4c3e0d14b98b17adc02cd06b8b2ced88b8fec
Aditya Kalambe <adityakalambe20@gmail.com>
2024-04-12 07:50:57 +0530

    Course CRUD Operation Repository update

--- src/main/java/com/attendancemanagementsystem/repository/CourseRepository.java
+++ src/main/java/com/attendancemanagementsystem/repository/CourseRepository.java
@@ -1,8 +1,9 @@
 package com.attendancemanagementsystem.repository;
 
+import com.attendancemanagementsystem.entity.Course;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface Course extends JpaRepository<Course, Long> {
+public interface CourseRepository extends JpaRepository<Course, Long> {
 }
