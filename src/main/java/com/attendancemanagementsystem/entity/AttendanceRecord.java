c83507f67b7a664eb9b734e1f60b87238ae96095
Aditya Kalambe <adityakalambe20@gmail.com>
2024-04-12 07:14:53 +0530

    Attendance Entity

---
+++ src/main/java/com/attendancemanagementsystem/entity/AttendanceRecord.java
@@ -0,0 +1,30 @@
+package com.attendancemanagementsystem.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "course")
+public class AttendanceRecord {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "student_id")
+    private Long studentId;
+
+    @Column(name = "course_id")
+    private Long courseId;
+
+    @Column(name = "attendance_date")
+    private LocalDate attendanceDate;
+
+    @Column(name = "attendance_status")
+    private String attendanceStatus;
+
+}
