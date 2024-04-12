392cdb3f5233394750528bda119d7e870055f4e7
Aditya Kalambe <adityakalambe20@gmail.com>
2024-04-12 07:00:13 +0530

    Department Entity

---
+++ src/main/java/com/attendancemanagementsystem/entity/Department.java
@@ -0,0 +1,23 @@
+package com.attendancemanagementsystem.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "course")
+public class Department {
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long departmentId;
+
+    @Column(name = "name")
+    private String departmentName;
+
+
+}
