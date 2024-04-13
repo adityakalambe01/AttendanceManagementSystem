853c143ee4486b143709cccf73023304b6540ff2
Aditya Kalambe <adityakalambe20@gmail.com>
2024-04-13 23:33:35 +0530

    User Repository Update

--- src/main/java/com/attendancemanagementsystem/repository/UserRepository.java
+++ src/main/java/com/attendancemanagementsystem/repository/UserRepository.java
@@ -4,9 +4,15 @@ import com.attendancemanagementsystem.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
     User findByUsername(String username);
 
     User findByUserEmailId(String userEmailId);
+
+    List<User> findByUserFullNameContainingIgnoreCase(String name);
+
+    List<User> findByUserRoleContainingIgnoreCase(String role);
 }
