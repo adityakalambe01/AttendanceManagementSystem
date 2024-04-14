9edf8282c378cb5b62907a853a48fa8804e7262c
Aditya Kalambe <adityakalambe20@gmail.com>
2024-04-14 17:30:32 +0530

    Attendance Record Repository

--- src/main/java/com/attendancemanagementsystem/repository/AttendanceRecordRepository.java
+++ src/main/java/com/attendancemanagementsystem/repository/AttendanceRecordRepository.java
@@ -4,6 +4,12 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.attendancemanagementsystem.entity.AttendanceRecord;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDate;
+import java.util.List;
+
 @Repository
 public interface AttendanceRecordRepository extends JpaRepository<AttendanceRecord, Long> {
+    List<AttendanceRecord> findByAttendanceDate(LocalDate localDate);
+
+    List<AttendanceRecord> findByAttendanceDateAndAttendanceStatus(LocalDate localDate, String attendanceStatus);
 }
