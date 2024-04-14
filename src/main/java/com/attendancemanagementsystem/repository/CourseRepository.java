4d7d94a1242dcf226e9747e8afdbdae550e83142
Aditya Kalambe <adityakalambe20@gmail.com>
2024-04-14 17:31:13 +0530

    Course Repository

--- src/main/java/com/attendancemanagementsystem/repository/CourseRepository.java
+++ src/main/java/com/attendancemanagementsystem/repository/CourseRepository.java
@@ -4,6 +4,11 @@ import com.attendancemanagementsystem.entity.Course;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface CourseRepository extends JpaRepository<Course, Long> {
+    List<Course> findByCourseName(String courseName);
+
+    List<Course> findByDepartmentId(Long departmentId);
 }
