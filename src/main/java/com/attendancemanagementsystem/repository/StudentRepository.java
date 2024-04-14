89479f5a47d4d2a6841ec39eb3416fd4ddc2121f
Aditya Kalambe <adityakalambe20@gmail.com>
2024-04-14 17:33:08 +0530

    Student Repository Update

--- src/main/java/com/attendancemanagementsystem/repository/StudentRepository.java
+++ src/main/java/com/attendancemanagementsystem/repository/StudentRepository.java
@@ -1,9 +1,11 @@
 package com.attendancemanagementsystem.repository;
 
 import com.attendancemanagementsystem.entity.Student;
+import com.attendancemanagementsystem.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface StudentRepository extends JpaRepository<Student, Long> {
+    Student findByStudentEmailId(String studentEmailId);
 }
