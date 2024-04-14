c5c8aa39fdaaf195c1d4c85f60f33452c34a5672
Aditya Kalambe <adityakalambe20@gmail.com>
2024-04-14 17:58:10 +0530

    Faculty Exception Update

---
+++ src/main/java/com/attendancemanagementsystem/customexceptions/FacultyException.java
@@ -0,0 +1,11 @@
+package com.attendancemanagementsystem.customexceptions;
+
+public class FacultyException extends Exception{
+    public FacultyException(){
+        super();
+    }
+
+    public FacultyException(String message){
+        super(message);
+    }
+}
