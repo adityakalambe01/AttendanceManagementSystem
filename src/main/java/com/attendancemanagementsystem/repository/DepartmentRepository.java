8f0c59b507238a87f1d4fa25b8042394dbf1aafb
Aditya Kalambe <adityakalambe20@gmail.com>
2024-04-14 17:32:03 +0530

    Department Repository Update

--- src/main/java/com/attendancemanagementsystem/repository/DepartmentRepository.java
+++ src/main/java/com/attendancemanagementsystem/repository/DepartmentRepository.java
@@ -4,6 +4,11 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import com.attendancemanagementsystem.entity.Department;
 
+import java.util.List;
+
 @Repository
 public interface DepartmentRepository extends JpaRepository<Department, Long> {
+    Department findByDepartmentName(String departmentName);
+
+    List<Department> findByDepartmentNameContainingIgnoreCase(String departmentName);
 }
