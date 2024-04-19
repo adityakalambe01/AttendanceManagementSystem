e3d7b4ae5bc8984fdfbf8813e936b57ef4a5f4bb
Aditya Kalambe <adityakalambe20@gmail.com>
2024-04-19 12:56:23 +0530

    User Controller

--- src/main/java/com/attendancemanagementsystem/controller/UserController.java
+++ src/main/java/com/attendancemanagementsystem/controller/UserController.java
@@ -5,8 +5,11 @@ import com.attendancemanagementsystem.entity.User;
 import com.attendancemanagementsystem.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
 
 @Controller
 @RequestMapping("users/")
@@ -18,6 +21,11 @@ public class UserController {
     PageRedirect redirect;
 
 
+    /*
+    *
+    * User Signup
+    *
+    * */
     @PostMapping("signUp")
     public String addNewUser(User newUser){
         if(userService.addNewUser(newUser)){
@@ -26,6 +34,11 @@ public class UserController {
         return redirect.userSignUpPage();
     }
 
+    /*
+    *
+    * User Login
+    *
+    * */
     @PostMapping("login")
     public String userLogin(User user){
         User userLogin = userService.userLogin(user);
@@ -39,6 +52,61 @@ public class UserController {
             //wrong password entered
             return redirect.userSignInPage();
         }
+    }
+
+    /*
+    *
+    * Delete User
+    *
+    * */
+    @GetMapping("delete")
+    public String deleteUser(Long userId){
+        return userService.deleteUser(userId) ? "":"";
+    }
+
+    /*
+    *
+    * Update User
+    *
+    * */
+    @PostMapping("update")
+    public String updateUser(Long userId, User updateUser){
+        User userData = userService.updateExistingUser(userId, updateUser);
+        return "";
+    }
+
+    /*
+    *
+    * Get User By Id
+    *
+    * */
+    @GetMapping("id")
+    public String getUserById(Long userId){
+        User userData = userService.getUserById(userId);
+        return "";
+    }
+
+    /*
+    *
+    * List of User By Name
+    *
+    * */
+    @GetMapping("userList")
+    public String getUserByName(String name){
+        List<User> userList = userService.getUserByName(name);
+
+        return "";
+    }
+
+    /*
+    *
+    * List of User by Role
+    *
+    * */
+    @GetMapping("listByRole")
+    public String getUserByRole(String role){
+        List<User> userList = userService.getUserByRole(role);
+        return "";
     }
 
 }
