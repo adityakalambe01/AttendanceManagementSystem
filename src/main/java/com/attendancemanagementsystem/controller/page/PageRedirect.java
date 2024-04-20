1e3382b5c1c6261ecbb4f4589506759f46de5d29
Aditya Kalambe <adityakalambe20@gmail.com>
2024-04-21 02:31:02 +0530

    Page Redirect

--- src/main/java/com/attendancemanagementsystem/controller/page/PageRedirect.java
+++ src/main/java/com/attendancemanagementsystem/controller/page/PageRedirect.java
@@ -1,6 +1,5 @@
 package com.attendancemanagementsystem.controller.page;
 
-import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -11,6 +10,66 @@ public class PageRedirect {
         return "index";
     }
 
+    /*
+    *
+    * Admin
+    *
+    * */
+    //auth
+    @RequestMapping({"adminSignUp", "signUpAdmin"})
+    public String adminSignUp(){
+        return "admin/auth/signup";
+    }
+
+    @RequestMapping({"adminLogin","loginAdmin"})
+    public String loginAdmin(){
+        return "admin/auth/login";
+    }
+
+    //dashboard main
+    @RequestMapping({"adminDashboard","dashboardAdmin"})
+    public String adminDashboard(){
+        return "admin/dashboard";
+    }
+
+    //department
+    @RequestMapping({"addDepartment","addNewDepartment","newDepartment","new_dept"})
+    public String addNewDepartment(){
+        return "admin/department/add";
+    }
+
+    @RequestMapping({"viewDepartment","showDepartment","view_dept"})
+    public String viewDepartment(){
+        return "admin/department/view";
+    }
+
+    //faculty
+    @RequestMapping({"addFaculty","addNewFaculty","newFaculty","new_fact"})
+    public String addNewFaculty(){
+        return "admin/faculty/add";
+    }
+
+    @RequestMapping({"viewFaculty","showFaculty","view_fact"})
+    public String viewFaculty(){
+        return "admin/faculty/view";
+    }
+
+    //student
+    @RequestMapping({"addStudent","addNewStudent","newStudent","new_stud"})
+    public String addNewStudent(){
+        return "admin/student/add";
+    }
+
+    @RequestMapping({"viewStudent","showStudent","view_stud"})
+    public String viewStudent(){
+        return "admin/student/view";
+    }
+
+
+
+
+
+//
     @RequestMapping("userSignUp")
     public String userSignUpPage(){
         return "signup";
